2932afd2b1918753b75fe34a45d165e0e52986e6
Fred Ojure <fredojure@hotmail.com>
2020-06-08 20:19:36 +0300

    Added goals array to goal component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,14 +7,7 @@ import { Goal } from './goal';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  goals: Goal[] = [
-    {id:1, name: 'Watch Finding Nimo'},
-    {id:2, name: 'Buy Cookies'},
-    {id:3, name: 'Get new Phone case'},
-    {id:4, name: 'Get Dog food'},
-    {id:5, name: 'Solve Math Homework'},
-    {id:6, name: 'Plot my world domination plan'},
-  ]
+  
 
 
 }
