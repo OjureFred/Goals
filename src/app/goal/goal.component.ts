92edf12010fa15ab53106c0f185b74660934cad1
Fred Ojure <fredojure@hotmail.com>
2020-06-08 20:27:55 +0300

    Generated goal-detail component

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,12 +8,12 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
-    {id:1, name: 'Watch Finding Nimo'},
-    {id:2, name: 'Buy Cookies'},
-    {id:3, name: 'Get new Phone case'},
-    {id:4, name: 'Get Dog food'},
-    {id:5, name: 'Solve Math Homework'},
-    {id:6, name: 'Plot my world domination plan'},
+    {id:1, name: 'Watch Finding Nimo', description: 'Find an online version and watch Merlin find his son'},
+    {id:2, name: 'Buy Cookies', description: 'I have to buy cookies for the parrot'},
+    {id:3, name: 'Get new Phone case', description: 'Diana has her birthday coming up soon'},
+    {id:4, name: 'Get Dog food', description: 'Pupper likes expensive snacks'},
+    {id:5, name: 'Solve Math Homework', description: 'Damn math'},
+    {id:6, name: 'Plot my world domination plan', description: 'Cause I am an evil overload'},
   ]
 
   constructor() { }
