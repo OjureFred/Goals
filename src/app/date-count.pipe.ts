ec04a711b827e90207ce06b722094860aa6cb866
Fred Ojure <fredojure@hotmail.com>
2020-06-09 20:43:47 +0300

    Added code for date count pipe

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -5,8 +5,18 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DateCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+    transform(value: any): number {
+      let today: Date = new Date(); // get current date and nativeElement
+      let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+      var dateDifference = Math.abs(value - todayWithNoTime); //diffference in milliseconds
+      const secsInDay = 86400; //60 * 60 * 24
+      var dateDifferenceSeconds = dateDifference * 0.001; //convert to milliseconds
+      var dateCounter = dateDifferenceSeconds/secsInDay;
 
-}
+      if(dateCounter >= 1 && value > todayWithNoTime) {
+        return dateCounter;
+      } else {
+        return 0;
+      }
+    }
+  }
