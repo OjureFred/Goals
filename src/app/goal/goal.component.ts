d92acfd009e12995d598d0fc254afbd8424bbaf8
Fred Ojure <fredojure@hotmail.com>
2020-06-09 20:30:44 +0300

    Generated pipe for days count

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -8,12 +8,12 @@ import { Goal } from '../goal';
 })
 export class GoalComponent implements OnInit {
   goals: Goal[] = [
-    new Goal(1, 'Watch Finding Nimo', 'Find an online version and watch Merlin find his son'),
-    new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot'),
-    new Goal(3, 'Get new Phone case', 'Diana has her birthday coming up soon'),
-    new Goal(4, 'Get Dog food', 'Pupper likes expensive snacks'),
-    new Goal(5, 'Solve Math Homework', 'Damn math'),
-    new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overload'),
+    new Goal(1, 'Watch Finding Nimo', 'Find an online version and watch Merlin find his son', new Date(2020, 3, 14)),
+    new Goal(2, 'Buy Cookies', 'I have to buy cookies for the parrot', new Date(2019, 6, 9)),
+    new Goal(3, 'Get new Phone case', 'Diana has her birthday coming up soon', new Date(2022, 1, 12)),
+    new Goal(4, 'Get Dog food', 'Pupper likes expensive snacks', new Date(2019, 10, 18)),
+    new Goal(5, 'Solve Math Homework', 'Damn math', new Date(2019, 2, 14)),
+    new Goal(6, 'Plot my world domination plan', 'Cause I am an evil overload', new Date(2030, 3, 14)),
   ];
 
   toggleDetails(index){
